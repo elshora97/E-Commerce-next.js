b1386d85270239308e54899ee26839eae3a5c783
Ahmed Elshora <ahmedashrafelshora@gmail.com>
2025-09-21 03:24:36 +0300

    add product list comp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 export default async function HomePage() {
-  return <>prostore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrival"
+        limit={4}
+      />
+    </>
+  );
 }
