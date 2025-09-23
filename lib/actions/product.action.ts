9c1f9d0db6216a82c54c2bad821f07df1817b303
Ahmed Elshora <ahmedashrafelshora@gmail.com>
2025-09-24 01:44:05 +0300

    add product details page

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -2,7 +2,6 @@ import { LATEST_PRODUCTS_LIMIT } from '../constants';
 import { prisma } from '../../db/prisma';
 
 //get latest products
-
 export const getLatestProducts = async () => {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
@@ -10,4 +9,11 @@ export const getLatestProducts = async () => {
   });
 
   return data;
+};
+
+//get singly prouct by its slug
+export const getProductBySlug = async (slug: string) => {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 };
